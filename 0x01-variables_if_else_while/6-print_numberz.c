bdd31803d7758a30dd6953f940b3ff572e5176ae
Olaifa Timohy <olaifatimothy@gmail.com>
2023-06-19 11:09:27 -0700

    print numbers with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'print string value'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int alpha;
+
+	for (alpha = 48 ; alpha <= 57 ; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar(10);
+	return (0);
+}
