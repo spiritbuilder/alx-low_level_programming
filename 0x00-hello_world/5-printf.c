c324bf8c8de58a10e3c6e1a643752a1860de37c0
Olaifa Timohy <olaifatimothy@gmail.com>
2023-06-19 05:14:11 -0700

    print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'print a string'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
