5062f2cecaab05df64a7402c2cb3ac0da6fb1020
Olaifa Timohy <olaifatimothy@gmail.com>
2023-06-19 04:01:05 -0700

    fix puts program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    puts("\"Programming is like building a multilingual puzzle\n");
-    return 0;
+/**
+ * main - Entry point
+ *
+ * Description: 'print a string'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
