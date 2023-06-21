c91adc4e0f78e5a2b91efe9d142e20419b38a60f
Olaifa Timohy <olaifatimothy@gmail.com>
2023-06-20 22:00:26 -0700

    add _abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+
+/**
+ * _abs - Entry point
+ *@c: Character to be checked
+ *
+ * Description: 'print string value'
+ *
+ * Return: Always for success and 0 for false
+ *
+ */
+
+int  _abs(int c)
+{
+	return (abs(c));
+}
+
+
+
+
