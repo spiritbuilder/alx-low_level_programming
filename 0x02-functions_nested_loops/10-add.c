9f52dfebf04f0c1e2e03842513de81738464b07b
Olaifa Timohy <olaifatimothy@gmail.com>
2023-06-21 12:56:40 -0700

    add adder

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+* add - iterates condition
+* @a: First addition
+* @b: Second Addition
+*
+* Return: Returns an addition
+*/
+
+
+int add(int a, int b)
+{
+	return (a + b);
+}
